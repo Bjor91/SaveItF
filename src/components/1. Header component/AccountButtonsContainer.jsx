f49695d6049afd2f21b83a4d17765f7b710eb046
Bjorn Hagen <bjornhagen3@live.no>
2023-10-12 18:31:54 +0200

    formatting

--- src/components/1. Header component/AccountButtonsContainer.jsx
+++ src/components/1. Header component/AccountButtonsContainer.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 
-import AccountButton  from "./AccountButton.jsx";
-
+import AccountButton from "./AccountButton.jsx";
 
 function AccountButtonsContainer({ accountData, handleSetConstant }) {
-    return (
-      <div className="button-container">
-        {accountData.map(account => (
-          <AccountButton key={account.id} account={account} onClick={handleSetConstant} />
-        ))}
-      </div>
-    );
-  }
-  
-  export default AccountButtonsContainer;
+  return (
+    <div className="button-container">
+      {accountData.map((account) => (
+        <AccountButton
+          key={account.id}
+          account={account}
+          onClick={handleSetConstant}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default AccountButtonsContainer;
